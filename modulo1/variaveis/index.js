8a0de3d5b0db54eb243ac69b1184b261920ae9d5
Silvia Cristina Marques Viadanna <silviaviadanna@gmail.com>
2022-02-14 22:49:05 -0300

    Exercios Variaveis Incompletos

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,26 @@
+/* Exercícios de interpretação de código
+1. 10,5
+2. 10,10,10
+3. O progarama calcula o quanto voce recebe por hora
+p = horasTrabalhadasPorDia
+t= salarioaPor Dia
+*/
+
+let nome
+let idade
+console.log(typeof nome)
+console.log(typeof idade)
+
+/* undefinied para ambos , pois não definimos de number, string ou boleaan*/
+
+
+nome=   prompt("Digite seu nome: ")
+idade=   prompt("Digite seu idade: ")
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+/* retornam em valor de String, pois vieram através de um formulério*/
+
+console.log ("Olá ", nome,  "você tem: " , idade, "anos")
+
