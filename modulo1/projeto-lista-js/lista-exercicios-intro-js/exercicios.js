fcf46232db2994a81f5df202e2e7c3f48e896aea
Silvia Cristina Marques Viadanna <silviaviadanna@gmail.com>
2022-02-20 07:21:11 -0300

    Exercicios intro js

--- modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
@@ -1,10 +1,10 @@
-// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+//EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
 
 // EXERCÍCIO 0A
 
 // function soma(num1, num2) {
   
-//   // implemente sua lógica aqui
+  // implemente sua lógica aqui
 // }
 
 // // EXERCÍCIO 0B
@@ -37,132 +37,132 @@
  
 
 //  EXERCÍCIO 03
-// function calculaIMC(peso, altura) {
-//   const imc = peso/(altura*altura)
-//   return imc   
+function calculaIMC(peso, altura) {
+  const imc = peso/(altura*altura)
+  return imc   
 
-// }
-// // const peso= Number(prompt(`Digite seu peso: `))
-//   const altura = Number(prompt(`Digite sua altura: `))
-//   const mostraTelaimc = calculaIMC(peso,altura)
+}
+ const peso= Number(prompt(`Digite seu peso: `))
+  const altura = Number(prompt(`Digite sua altura: `))
+  const mostraTelaimc = calculaIMC(peso,altura)
 
-//   console.log(mostraTelaimc) 
+  console.log(mostraTelaimc) 
 
 
 
 //    EXERCÍCIO 04
-// function imprimeInformacoesUsuario() {
+function imprimeInformacoesUsuario() {
     
-//   const nome= prompt("Digite seu nome: ")
-//   const idade = Number(prompt("Digite sua idade: "))
-//   const email = prompt ("Digite seu email: ")
+  const nome= prompt("Digite seu nome: ")
+  const idade = Number(prompt("Digite sua idade: "))
+  const email = prompt ("Digite seu email: ")
 
 
-//  console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
-// }
+ console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
+}
 
 
 
 
 //   EXERCÍCIO 05
-// function imprimeTresCoresFavoritas() {
-//   const cor1=prompt("Digite sua 1a cor preferida")
-//   const cor2=prompt("Digite sua 2a cor preferida")
-//   const cor3=prompt("Digite sua 3a cor preferida")
-// const cores=[cor1,cor2,cor3]
+function imprimeTresCoresFavoritas() {
+  const cor1=prompt("Digite sua 1a cor preferida")
+  const cor2=prompt("Digite sua 2a cor preferida")
+  const cor3=prompt("Digite sua 3a cor preferida")
+const cores=[cor1,cor2,cor3]
 
-// console.log(cores)
+console.log(cores)
 
 
-//}
+}
 
 // EXERCÍCIO 06
-// function retornaStringEmMaiuscula(string) {
-//     const novoNome = string.toUpperCase()
-//     return novoNome
-//    }
+function retornaStringEmMaiuscula(string) {
+    const novoNome = string.toUpperCase()
+    return novoNome
+   }
 
 
 //    EXERCÍCIO 07
-// function calculaIngressosEspetaculo(custo, valorIngresso) {
-//     let venda=(custo/valorIngresso)
-//     return venda 
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+    let venda=(custo/valorIngresso)
+    return venda 
     
-// }
+}
     
 
 //   EXERCÍCIO 08
-// function checaStringsMesmoTamanho(string1, string2) {
-//   const tam1 = string1.length
-//   const tam2 = string2.length
-//   const condicao = tam1===tam2
-//   return condicao
-// }
+function checaStringsMesmoTamanho(string1, string2) {
+  const tam1 = string1.length
+  const tam2 = string2.length
+  const condicao = tam1===tam2
+  return condicao
+}
 
 
 //  EXERCÍCIO 09
-// function retornaPrimeiroElemento(array) {
-//   return array[0]
-//   }
-//   console.log(retornaPrimeiroElemento(["Laranja", "Banana", "Maçã"]))
+function retornaPrimeiroElemento(array) {
+  return array[0]
+  }
+  console.log(retornaPrimeiroElemento(["Laranja", "Banana", "Maçã"]))
 
 
 
 
 
 //    EXERCÍCIO 10
-// function retornaUltimoElemento(array) {
+function retornaUltimoElemento(array) {
   
-//   nrelementos = array.length
-//   return array[nrelementos-1]
+  nrelementos = array.length
+  return array[nrelementos-1]
 
-// }
-// console.log(retornaUltimoElemento(["Laranja", "Banana", "Maçã"]))
+}
+console.log(retornaUltimoElemento(["Laranja", "Banana", "Maçã"]))
 
 
 //       EXERCÍCIO 11
-// function trocaPrimeiroEUltimo(array) {
+function trocaPrimeiroEUltimo(array) {
   
-//   let prim= array[0]
-//    nrelementos = array.length
-//   let ulti= array[nrelementos-1]
-// let aux= ulti
-// let ult= prim  
-// array.pop()  // exclui ultimo elemento
-// array.push(ult) // adiciona ultimo elemento
-// array.shift()  // exclui 1o elemento
-// array.unshift(aux) // adidiona 1o elemento
+  let prim= array[0]
+   nrelementos = array.length
+  let ulti= array[nrelementos-1]
+let aux= ulti
+let ult= prim  
+array.pop()  // exclui ultimo elemento
+array.push(ult) // adiciona ultimo elemento
+array.shift()  // exclui 1o elemento
+array.unshift(aux) // adidiona 1o elemento
 
-// return array
+return array
 
-// }
+}
 
-//console.log(trocaPrimeiroEUltimo([aux, "Banana", prim]))
+console.log(trocaPrimeiroEUltimo([aux, "Banana", prim]))
 
 
-//    OOOOOKKKKKKK  EXERCÍCIO 12
-// function checaIgualdadeDesconsiderandoCase(string1, string2) {
+//  EXERCÍCIO 12
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
   
-//   string1=string1.toLowerCase()
-//   string2=string2.toLowerCase()
+  string1=string1.toLowerCase()
+  string2=string2.toLowerCase()
 
-//   const condicao = string1 === string2
-// return condicao
-// }
+  const condicao = string1 === string2
+return condicao
+}
 
 // EXERCÍCIO 13
-// function checaRenovacaoRG() {
+function checaRenovacaoRG() {
   
-//    anoAtual = Number(prompt(" Em que ano estamos? "))
-//   anoNasc = Number(prompt(" Em que ano você nasceu? "))
-//    anoEmiss = Number(prompt(" Qual ano de emissão? "))
+   anoAtual = Number(prompt(" Em que ano estamos? "))
+  anoNasc = Number(prompt(" Em que ano você nasceu? "))
+   anoEmiss = Number(prompt(" Qual ano de emissão? "))
  
-//    idade= anoAtual-anoNasc
-//  idadeRg= anoAtual-anoEmiss
-//   renova=( idade <= 20 && idadeRg >= 5) || ( (idade > 20 && idade <= 50) && idadeRg >= 10)|| (idade > 50 && idadeRg >= 15)
+   idade= anoAtual-anoNasc
+ idadeRg= anoAtual-anoEmiss
+  renova=( idade <= 20 && idadeRg >= 5) || ( (idade > 20 && idade <= 50) && idadeRg >= 10)|| (idade > 50 && idadeRg >= 15)
 
-//   console.log (renova)
-//  }
+  console.log (renova)
+ }
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
