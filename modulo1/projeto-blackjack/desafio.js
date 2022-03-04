fecbf9780e7667160c17a3b1054960e1dcbfa977
Silvia Cristina Marques Viadanna <silviaviadanna@gmail.com>
2022-03-04 15:20:48 -0300

    Meu Projeto Black Jack

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -10,4 +10,56 @@
  * 
  * 
  */
+    console.log("Boas vindas ao jogo de Blackjack!")
 
+    if(confirm("Quer iniciar uma nova rodada?")) {
+      let cartaUsuario1 = comprarCarta()
+      let cartaUsuario2 = comprarCarta()
+         if(cartaUsuario1.texto.includes("A") && cartaUsuario2.texto.includes("A") ) {
+            let cartaUsuario1 = comprarCarta();
+            let cartaUsuario2 = comprarCarta();
+          }
+          
+    somaUsuario=cartaUsuario1.valor + cartaUsuario2.valor
+
+      let cartaPc1 = comprarCarta()
+      let cartaPc2 = comprarCarta()
+
+        if(cartaPc1.texto.includes("A") && cartaPc2.texto.includes("A") ) {
+
+         let cartaPc1 = comprarCarta();
+         let cartaPc2 = comprarCarta();
+
+        }
+    somaPc=cartaPc1.valor + cartaPc2.valor
+    cartaPc= cartaPc1.texto + cartaPc2.texto
+       cartaUsuario= cartaUsuario1.texto + cartaUsuario2.texto
+
+    while(confirm(`Suas cartas são: ${cartaUsuario} A carta revelada do computador é ${cartaPc1.texto}
+      \n Deseja comprar mais uma carta?`)&& somaUsuario<21)  {
+         let novaCarta=comprarCarta()         
+          cartaUsuario=cartaUsuario + novaCarta.texto
+          somaUsuario=somaUsuario+novaCarta.valor
+    }
+
+    while(somaPc<21 && somaUsuario<=21)  {
+         let novaCartaPc=comprarCarta()         
+          cartaPc=cartaPc + novaCartaPc.texto
+          somaPc=somaPc+novaCartaPc.valor
+      }
+   
+   console.log(`Usuario - cartas: ${cartaUsuario} - pontuação: ${somaUsuario}`)
+
+   console.log("Computador - cartas: ", cartaPc, " - pontuação: " , somaPc)
+      
+if(somaUsuario<=21 && somaPc <somaUsuario ||(somaPc>21)) {
+      console.log("O usuário ganhou!")
+   } else if(somaUsuario<somaPc && somaPc<=21 ||(somaUsuario>21)) {    console.log("O Computador ganhou!")
+}
+   else {
+      console.log("Empate")
+   }
+         } else {
+            console.log("O jogo acabou")
+         }
+         console.log("O jogo acabou")
--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,55 +10,125 @@
  * 
  * ""
  */
-    console.log("Boas vindas ao jogo de Blackjack!")
-    if( confirm ("Quer iniciar uma nova rodada?"))  {
-        let cartaPc = []
-    cartaPc.push(comprarCarta())
-    cartaPc.push(comprarCarta())
-    let cartaUsuario=[]
-    cartaUsuario.push(comprarCarta())
-    cartaUsuario.push(comprarCarta())
-    while(cartaPc[0].texto.includes("A")&& cartaPc[1].texto.includes("A")){
-      cartaPc[0] = comprarCarta() 
-       cartaPc[1] = comprarCarta()  
-   }
-    while(cartaUsuario[0].texto.includes("A")&& cartaUsuario[1].texto.includes("A")) {
-    cartaUsuario[0] = comprarCarta() 
-    cartaUsuario[1] = comprarCarta()  
-    }
-    let pontoPc =cartaPc[0].valor + cartaPc[1].valor
-    let pontoUsuario =cartaUsuario[0].valor + cartaUsuario[1].valor
-    let loop=true
+// O JOGO SIMPLIFICADO
+
+//     console.log("Boas vindas ao jogo de Blackjack!")
+
+//     if(confirm("Quer iniciar uma nova rodada?")) {
+//       let cartaUsuario1 = comprarCarta();
+//       let cartaUsuario2 = comprarCarta();
+          
+//     somaUsuario=cartaUsuario1.valor + cartaUsuario2.valor
+// console.log("Usuario - cartas: ", cartaUsuario1.texto , cartaUsuario2.texto , " - pontuação: " , somaUsuario)
+
+//       let cartaPc1 = comprarCarta();
+//       let cartaPc2 = comprarCarta();
+          
+//     somaPc=cartaPc1.valor + cartaPc2.valor
+// console.log("Computador - cartas: ", cartaPc1.texto , cartaPc2.texto , " - pontuação: " , somaPc)
+
+// if(somaUsuario>somaPc) {
+//    console.log("O usuário ganhou!")
+// } else if(somaUsuario<somaPc) {    console.log("O Computador ganhou!")
+// }
+// else {
+//    console.log("Empate")
+// }
+//          } else {
+//             console.log ("O jogo acabou")
+//          }
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     console.log("Boas vindas ao jogo de Blackjack!")
+//     if( confirm ("Quer iniciar uma nova rodada?"))  {
+//         let cartaPc = []
+//     cartaPc.push(comprarCarta())
+//     cartaPc.push(comprarCarta())
+//     let cartaUsuario=[]
+//     cartaUsuario.push(comprarCarta())
+//     cartaUsuario.push(comprarCarta())
+//     while(cartaPc[0].texto.includes("A")&& cartaPc[1].texto.includes("A")){
+//       cartaPc[0] = comprarCarta() 
+//        cartaPc[1] = comprarCarta()  
+//    }
+//     while(cartaUsuario[0].texto.includes("A")&& cartaUsuario[1].texto.includes("A")) {
+//     cartaUsuario[0] = comprarCarta() 
+//     cartaUsuario[1] = comprarCarta()  
+//     }
+//     let pontoPc =cartaPc[0].valor + cartaPc[1].valor
+//     let pontoUsuario =cartaUsuario[0].valor + cartaUsuario[1].valor
+//     let loop=true
     
 
-let resultado=""
-let mensagemUsuario= ""
-let mensagemPc=""
+// let resultado=""
+// let mensagemUsuario= ""
+// let mensagemPc=""
 
-if(pontoUsuario>21 || pontoPc<=21 && pontoPc > pontoUsuario) {
+// if(pontoUsuario>21 || pontoPc<=21 && pontoPc > pontoUsuario) {
 
-resultado= "O computador ganhou"
-}
+// resultado= "O computador ganhou"
+// }
 
-else if(pontoUsuario===pontoPc) {
-   resultado= "Deu Empate"
+// else if(pontoUsuario===pontoPc) {
+//    resultado= "Deu Empate"
 
-}
-else { 
-   resultado= " O Usuáro ganhou" 
-}
- for (carta of cartaUsuario) {
-    mensagemUsuario += `${carta.texto}`
- }
- for (carta of cartaPc) {
-    mensagemPc += `${carta.texto}`
+// }
+// else { 
+//    resultado= " O Usuáro ganhou" 
+// }
+//  for (carta of cartaUsuario) {
+//     mensagemUsuario += `${carta.texto}`
+//  }
+//  for (carta of cartaPc) {
+//     mensagemPc += `${carta.texto}`
 
- }
-    console.log ("Usuário-Cartas = ", mensagemUsuario , " POntuação=  "  , pontoUsuario ,  "\n Computador - Cartas = "  , mensagemPc ,  "Pontuação = ", pontoPc  , "\n" ,resultado)
+//  }
+//     console.log ("Usuário-Cartas = ", mensagemUsuario , " POntuação=  "  , pontoUsuario ,  "\n Computador - Cartas = "  , mensagemPc ,  "Pontuação = ", pontoPc  , "\n" ,resultado)
 
-} else 
-{   console.log ( " O jogo acabou")
-}
+// } else 
+// {   console.log ( " O jogo acabou")
+// }
    
    
    
